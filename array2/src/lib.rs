5c0fbd76ba30b380529e6d7454140280531ca667
Ben Keeler <71103393+benkeeler12@users.noreply.github.com>
2023-10-07 22:31:44 -0400

    Update lib.rs
    updated lib.rs

--- array2/src/lib.rs
+++ array2/src/lib.rs
@@ -5,19 +5,26 @@ pub struct Array2<T> {
 }
 
 impl<T: Clone> Array2<T> {
-    // Row Major constructor
-    pub fn from_row_major(width: usize, height: usize, elements: Vec<T>,) -> Result<Self, &'static str> {
+        // Row Major constructor
+    pub fn from_row_major(
+        width: usize,
+        height: usize,
+        elements: Vec<T>,
+    ) -> Result<Self, &'static str> {
+        // ensure the board has valid number of elements
         if elements.len() != width * height {
             return Err("Invalid number of elements");
         }
-
+        // set a empty vec which will be filled with the inputted elements
         let mut data = Vec::with_capacity(width * height);
-
+        //  fill the vec
         for y in 0..height {
             for x in 0..width {
                 let index = y * width + x;
                 data.push(elements[index].clone());
             }
+
+        // construct our instance of array2 with the width, height, and vec 
         }
 
         Ok(Self {
@@ -28,28 +35,32 @@ impl<T: Clone> Array2<T> {
     }
 
     // Col-major constructor
-    pub fn from_col_major(width: usize, height: usize, elements: Vec<T>,) -> Result<Self, &'static str> {
+    pub fn from_col_major(
+        width: usize,
+        height: usize,
+        elements: Vec<T>,
+    ) -> Result<Self, &'static str> {
+        // ensure the board has valid number of elements
         if elements.len() != width * height {
             return Err("Invalid number of elements");
         }
-
+        // set a empty vec which will be filled with the inputted elements
         let mut data = Vec::with_capacity(width * height);
-
+        //  fill the vec
         for y in 0..width {
             for x in 0..height {
                 let index = y * width + x;
                 data.push(elements[index].clone());
             }
         }
-
+        // construct our instance of array2 with the width, height, and vec 
         Ok(Self {
             width,
             height,
             data,
         })
     }
-  
-    // 
+    // blank-state constructor if needed
     pub fn blank_state(width: usize, height: usize, val: T) -> Self {
         let data = vec![val; width * height];
 
@@ -60,13 +71,13 @@ impl<T: Clone> Array2<T> {
         }
     }
 
-
     // iterates over the rows
     pub fn iter_row_major(&self) -> impl Iterator<Item = (usize, usize, &T)> {
         (0..self.height).flat_map(move |y| (0..self.width).map(move |x| (x, y, self.get(x, y))))
     }
 
-    //iterates over the columns
+    //iterates over the columns skipping by the width to ensure we are going by column
+
     pub fn iter_col_major(&self) -> impl Iterator<Item = (usize, usize, &T)> {
         (0..self.width)
             .map(move |c| (c, self.data.iter().skip(c)))
@@ -77,17 +88,17 @@ impl<T: Clone> Array2<T> {
             })
     }
 
-    //function to get index from pair of coordinates
+    //function to return element from a pair of coordinates
     pub fn get(&self, x: usize, y: usize) -> &T {
         assert!(x < self.width);
         assert!(y < self.height);
         &self.data[x + y * self.width]
     }
-
+    // way to access width
     pub fn width(&self) -> usize {
         self.width
     }
-
+    // way to access height
     pub fn height(&self) -> usize {
         self.height
     }
